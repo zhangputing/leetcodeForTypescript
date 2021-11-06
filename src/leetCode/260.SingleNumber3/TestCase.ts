6cf8c45bd70b1c909c3f26848560c34f8297c59c
Max Zhang <zhangputing@vip.qq.com>
2021-11-06 10:54:34 +0800

    fix case

--- src/leetCode/260.SingleNumber3/TestCase.ts
+++ src/leetCode/260.SingleNumber3/TestCase.ts
@@ -1,14 +1,11 @@
 import { TestBase } from "../../FunctionBase/FunctionBase";
 import { CodeMap } from "../../test/CodeMap";
-class TestCase extends TestBase<number[], number[]> {
+class TestCase extends TestBase<[number[]], number[]> {
     public getFuncName(): string {
         return "SingleNumber3";
     }
     @CodeMap.registerCase()
-    private case1(): [number[], number[]] {
-        return [
-            [1, 2, 1, 3, 2, 5],
-            [3, 5],
-        ];
+    private case1(): [[number[]], number[]] {
+        return [[[1, 2, 1, 3, 2, 5]], [3, 5]];
     }
 }
--- src/leetCode/9.Palindrome/TestCase.ts
+++ src/leetCode/9.Palindrome/TestCase.ts
@@ -1,28 +1,28 @@
 import { TestBase } from "../../FunctionBase/FunctionBase";
 import { CodeMap } from "../../test/CodeMap";
 
-class TestCase extends TestBase<number, boolean> {
+class TestCase extends TestBase<[number], boolean> {
     public getFuncName(): string {
         return "Palindrome";
     }
 
     @CodeMap.registerCase()
-    private case1(): [number, boolean] {
-        return [456, false];
+    private case1(): [[number], boolean] {
+        return [[456], false];
     }
 
     @CodeMap.registerCase()
-    private case2(): [number, boolean] {
-        return [454, true];
+    private case2(): [[number], boolean] {
+        return [[454], true];
     }
 
     @CodeMap.registerCase()
-    private case3(): [number, boolean] {
-        return [45678, false];
+    private case3(): [[number], boolean] {
+        return [[45678], false];
     }
 
     @CodeMap.registerCase()
-    private case4(): [number, boolean] {
-        return [8456548, true];
+    private case4(): [[number], boolean] {
+        return [[8456548], true];
     }
 }
